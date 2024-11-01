3c2036195a3ceb1eefc4e435abda6e4a1129244b
K0GSxz <166021114+K0GSxz@users.noreply.github.com>
2024-11-01 10:19:48 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -21,7 +21,65 @@ async function quantidadeUsuariosPorRede() {
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color')
+,
+        titulo{// código omitido
 
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo'
+  }
+}
+
+// código omitido
+        ,
+    // código omitido
+
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0
+  }
+}
+
+// código omitido
+        title{// código omitido
+
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color')
+     }
+  }
+}
+
+// código omitido
+            ,
+    family: getCSS('--font')
+            // código omitido
+
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
+     }
+  }
+}
+
+// código omitido
     }
 
     const grafico = document.createElement('div')
